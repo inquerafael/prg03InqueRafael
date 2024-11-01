fbec8fcd86c1838cccc971300f6ad685f6f5e423
Inque <Inque@WIN-31BE1Q5I0TC>
2024-11-01 16:11:01 -0300

    teste de inicializaçao do repositorio

---
+++ prg03Inquerafael/src/main/java/br/com/iTa/a1vidade01/inquerafael/Prg03Inquerafael.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.iTa.a1vidade01.inquerafael;
+
+/**
+ *
+ * @author Inque
+ */
+public class Prg03Inquerafael {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
