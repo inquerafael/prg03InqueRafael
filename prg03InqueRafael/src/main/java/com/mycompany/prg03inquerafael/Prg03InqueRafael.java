266243ea91cdfd4ee3680b23d3d861457aa57a18
Inque <Inque@WIN-31BE1Q5I0TC>
2024-11-01 14:41:16 -0300

    teste de inicializaçao do repositorio

---
+++ prg03InqueRafael/src/main/java/com/mycompany/prg03inquerafael/Prg03InqueRafael.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prg03inquerafael;
+
+/**
+ *
+ * @author Inque
+ */
+public class Prg03InqueRafael {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
