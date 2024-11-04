00775b004d7f65f059a42f9830d37dd23077d27e
Inque <playerong@gmail.com>
2024-11-03 22:25:48 -0300

    teste de inicializaçao do repositorio

---
+++ prg03InquRafael/src/main/java/br/com/atividade01/Inque/Prg03InquRafael.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.atividade01.Inque;
+
+/**
+ *
+ * @author Inque
+ */
+public class Prg03InquRafael {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
