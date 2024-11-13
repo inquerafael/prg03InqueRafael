70aeb40f032142bdf10281c2ea4f7facd040259e
Inque <playerong@gmail.com>
2024-11-13 12:40:44 -0300

    / comentario da funcao

---
+++ prg04InqueRafael/src/prg04inquerafael/Prg04InqueRafael.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prg04inquerafael;
+
+/**
+ *
+ * @author Inque
+ */
+public class Prg04InqueRafael {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
